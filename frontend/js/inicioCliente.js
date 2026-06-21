b78d508bdd684efc641abbbef390d314ed00ca09
unknown <alexandertito121@gmail.com>
2026-06-21 01:29:22 -0300

    Actualizando vista Inicio Cliente. falta arreglar un error con respecto al guardado de tema y falta la funcionalidad de guardar nombre

--- frontend/js/inicioCliente.js
+++ frontend/js/inicioCliente.js
@@ -1,12 +1,26 @@
-let botonCambiarEstilo = document.querySelector(".btn-cambiar-estilo");
+const botonCambiarEstilo = document.querySelector(".btn-cambiar-estilo");
+const campoNombreUsuario = document.querySelector(".nombre-usuario")
+const NOMBRE_USUARIO = getNombreUsuario(); // Obtengo y guardo el nombre del usuario
+//export default NOMBRE_USUARIO; //Exporto el Nombre de usuario, facilitando la obtencion de nombre usuario para las demas clases 
 
 function init(){
-    actualizarModoNocturno(cargarEstilo());
+    iniciarTema();
 }
 
-function guardarEstiloALocalStorage(valor){
-    localStorage.setItem("modoNocturno", valor);
+
+/*-- Funcion que guarda al localStorage el nombre del usuario.  ---*/ 
+
+function guardarNombreUsuario(){
+    localStorage.setItem("nombre-usuario", campoNombreUsuario.value);
 }
+
+/*-- Funcion que obtiene el nombre del usuario del localStorage*/
+function getNombreUsuario(){
+    return localStorage.getItem("nombre-usuario") || "Nombre No definido";
+}
+
+
+/* Obtiene el valor booleano de la key "modoNocturno" y lo retorna */
 function cargarEstilo(){
     let modoNocturnoActivo = JSON.parse(localStorage.getItem("modoNocturno"));
     if(!modoNocturnoActivo){
@@ -14,17 +28,28 @@ function cargarEstilo(){
     }
     return modoNocturnoActivo;
 }
+
+
+/* 
+Cambia el valor booleano de modoNocturnoActivo. Actualiza los cambios y Guarda los el valor modificado al localStorage.
+Sin retorno */
+
 function cambiarEstilo(){
     modoNocturnoActivo = cargarEstilo();
-    if(modoNocturnoActivo == false){
-        modoNocturnoActivo = true;
+    if(modoNocturnoActivo){
+        modoNocturnoActivo = false;
     }
     else{
-        modoNocturnoActivo = false;
+        modoNocturnoActivo = true;
     }
     actualizarModoNocturno(modoNocturnoActivo);
     guardarEstiloALocalStorage(modoNocturnoActivo);
 }
+
+
+/* Actualiza el estilo de la pagina por el valor booleano recibido como parametro.
+Sin retorno*/
+
 function actualizarModoNocturno(modoNocturnoActivo){
     if(modoNocturnoActivo){
         estiloNocturno();
@@ -36,22 +61,29 @@ function actualizarModoNocturno(modoNocturnoActivo){
     }
 }
 
-function estiloNocturno(){
-    document.documentElement.style.setProperty("--color-fondo", "#000");
-    document.documentElement.style.setProperty("--color-texto","#fff");
-    document.documentElement.style.setProperty("--color-bordes","#fff");
-    document.documentElement.style.setProperty("--color-formulario-sombra","#fff");
-    document.documentElement.style.setProperty("--color-fondo-hover","#fff");
-    document.documentElement.style.setProperty("--color-texto-hover","#000");
 
+/* Cambia los colores a un estilo oscuro.
+Sin retorno*/
+
+function aplicarTema(tema) {
+    if (tema === 'claro') {
+        document.body.classList.add('tema-claro');
+    } else {
+        document.body.classList.remove('tema-claro');
+    }
 }
-function estiloNormal(){
-    document.documentElement.style.setProperty("--color-fondo", "#fff");
-    document.documentElement.style.setProperty("--color-texto","#000");
-    document.documentElement.style.setProperty("--color-bordes","#000");
-    document.documentElement.style.setProperty("--color-formulario-sombra","#000");
-    document.documentElement.style.setProperty("--color-fondo-hover","#000");
-    document.documentElement.style.setProperty("--color-texto-hover","#fff");
-    
+
+function iniciarTema() {
+    const temaGuardado = localStorage.getItem('tema') || 'oscuro';
+    const select = document.getElementById('tema-select');
+    select.value = temaGuardado;
+    console.log("HODFLGG")
+    aplicarTema(select.value);
+    select.addEventListener('change', () => {
+        const nuevoTema = select.value;
+        localStorage.setItem('tema', nuevoTema);
+        aplicarTema(nuevoTema);
+    });
 }
+
 init();
