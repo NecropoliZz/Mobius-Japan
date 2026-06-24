38ca5a6cdc90fb31ff1a7b7a722d1a03b0048f0c
unknown <alexandertito121@gmail.com>
2026-06-23 23:51:45 -0300

    Pantalla ya estelirizada. Faltaria hacer el header y la funcionalidad de guardar en excel y imprimir

--- frontend/js/ticket.js
+++ frontend/js/ticket.js
@@ -12,16 +12,16 @@ const PRODUCTOS = getProductos();
 //Renderizaciones
 
 function renderizarProductos(productos){
-    const tablaCompras = document.querySelector(".tabla-compras");
+    const tablaCompras = document.getElementById("tabla-compras");
     let htmlProductos = `<tr>
-                            <th>Producto</th>
-                            <th>Precio unitario</th>
+                            <th class="encabezado-tabla-productos">Producto</th>
+                            <th class="encabezado-tabla-productos">Subtotal</th>
                         </tr>`;
     productos.forEach(producto => {
         htmlProductos += 
         `<tr>
-            <td>${producto.nombre} x $${producto.precio} x ${producto.cantidad}</td>
-            <td>${getPrecioUnitario(producto)}</td>
+            <td class="dato-nombre-producto">${producto.nombre} x $${producto.precio} x ${producto.cantidad}</td>
+            <td class= "dato-subtotal">$${getPrecioSubtotal(producto)}</td>
         </tr>`;
     });
     tablaCompras.innerHTML = htmlProductos;
@@ -29,12 +29,12 @@ function renderizarProductos(productos){
 
 function renderizarUsuario(usuarioARenderizar){
     const usuario = document.getElementById("usuario");
-    usuario.innerHTML = `A nombre de ${usuarioARenderizar}`;
+    usuario.innerHTML = `Compra de <span>${usuarioARenderizar} </span>`;
 }
 
 function renderizarTotal(productos){
     const precioTotal = document.getElementById("precio-final");
-    precioTotal.innerHTML= `Total a pagar: $${getPrecioTotal(productos)}`
+    precioTotal.innerHTML= `Total a pagar: <span>$${getPrecioTotal(productos)}</span>`
 }
 function renderizarFecha(){
     const dia = document.getElementById("dia");
@@ -47,7 +47,7 @@ function renderizarFecha(){
 
 //HELPERS
 
-function getPrecioUnitario(producto){
+function getPrecioSubtotal(producto){
     return producto.precio * producto.cantidad;
 }
 function getNombreUsuario(){
@@ -57,12 +57,16 @@ function getNombreUsuario(){
 
 function getPrecioTotal(productos){
     let precioTotal = 0;
-    productos.forEach(producto => precioTotal += getPrecioUnitario(producto));
+    productos.forEach(producto => precioTotal += getPrecioSubtotal(producto));
     return precioTotal;
 }
 
 function getProductos(){
-    return JSON.parse(localStorage.getItem("carrito") || []);
+    let productos = localStorage.getItem("carrito") || [];
+    if(productos.length >0){
+        productos = JSON.parse(productos);
+    }
+    return productos;
 }
 
 init();
